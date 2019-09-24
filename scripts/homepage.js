216d3e34258e6e2abab2f93d748f4b69b8ab5dd6
bonkTK <54248765+bonkTK@users.noreply.github.com>
2019-09-25 00:13:25 +0200

    Add files via upload

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,45 +1,49 @@
-$('.owl-features').owlCarousel({
-	loop: true,
-	nav: true,
-	navText: [`<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
-	margin: 20,
-	autoplay: true,
-	autoplayTimeout: 5000,
-	smartSpeed: 1000,
-	dots: false,
-	responsiveClass: true,
-	responsive: {
-		0: {
-			items: 1,
-			nav: false
-		},
-		768: {
-			items: 2
-		}
-	}
-});
+$(function () {
 
-let sN = 0;
-const slg = [
+	$('.owl-features').owlCarousel({
+		loop: true,
+		nav: true,
+		navText: [`<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		smartSpeed: 1000,
+		dots: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: false
+			},
+			768: {
+				items: 2
+			}
+		}
+	});
+
+	let sN = 0;
+	const slg = [
 			['Welcome to', 'the bonk.tk'],
 			['Participate in', 'presitigious tournaments'],
 			['Compete with', 'best football players'],
 			['Play bonk.io in', 'a friendly atmosphere']
 		];
 
-let sloganText = () => {
-	$('#slogan-text').css("opacity", "1");
+	let sloganText = () => {
+		$('#slogan-text').css("opacity", "1");
 
-	let slgNumber = slg[sN];
-	$('#slogan-text').html(`<span>${slgNumber[0]}</span><span class="text-secondary">${slgNumber[1]}</span>`), sN++;
+		let slgNumber = slg[sN];
+		$('#slogan-text').html(`<span>${slgNumber[0]}</span><span class="text-secondary">${slgNumber[1]}</span>`), sN++;
 
-	if (sN === slg.length) {
-		sN = 0;
+		if (sN === slg.length) {
+			sN = 0;
+		};
+
+		setTimeout(() => {
+			$('#slogan-text').css("opacity", "0");
+		}, 4250);
 	};
 
-	setTimeout(() => {
-		$('#slogan-text').css("opacity", "0");
-	}, 4250);
-};
+	setInterval(sloganText, 5000);
 
-setInterval(sloganText, 5000);
+});
