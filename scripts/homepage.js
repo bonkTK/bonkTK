cda5bbf227cee748db6c5b57cee494cb4fe3dd8e
bonkTK <54248765+bonkTK@users.noreply.github.com>
2019-09-28 00:48:18 +0200

    Add files via upload

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -29,8 +29,25 @@ $(function () {
 			['Play bonk.io in', 'a friendly atmosphere']
 		];
 
+	let sloganBox = `
+	<div class="slogan-box">
+		<h2 id="slogan-text">
+			<span id="slogan-primary"></span>
+			<span id="slogan-secondary" class="text-secondary"></span>
+		</h2>
+	</div>
+	`;
+
+	$('.slogan').prepend(sloganBox);
+
+
+	$('#slogan-text').html(`<span>${slg[0][0]}</span><span class="text-secondary">${slg[0][1]}</span>`);
+
 	let sloganText = () => {
-		$('#slogan-text').css("opacity", "1");
+
+		setTimeout(() => {
+			$('#slogan-text').css("opacity", "1");
+		}, 25);
 
 		let slgNumber = slg[sN];
 		$('#slogan-text').html(`<span>${slgNumber[0]}</span><span class="text-secondary">${slgNumber[1]}</span>`), sN++;
