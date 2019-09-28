5409334e64b7f853120a2fc30f1927ccf6017772
bonkTK <54248765+bonkTK@users.noreply.github.com>
2019-09-29 01:15:05 +0200

    Update players.js

--- scripts/players.js
+++ scripts/players.js
@@ -19,14 +19,12 @@ const newPlayers = [
    new players('klajster', 'Poland'),
    new players('General_Richt', 'Russia'),
    new players('GSpeku', 'Germany'),
-   new players('vAZz', 'Germany'),
    new players('Bunniesss', 'United-States'),
    new players('Trocir', 'Serbia'),
    new players('bbbZG', 'Croatia'),
    new players('Pimpektron1', 'Serbia'),
    new players('leftright', 'Albania'),
    new players('Nub1', 'Lebanon'),
-   new players('LORDBAZ', 'Lebanon'),
    new players('TheAngryBull', 'United-States'),
    new players('SxC Swish', 'United-States'),
    new players('stussyg', 'United-States'),
@@ -34,8 +32,6 @@ const newPlayers = [
    new players('R3YM4Rjr', 'United-Kingdom'),
    new players('Victom', 'United-States'),
    new players('Nemenick', 'Italy'),
-   new players('anxiety', 'Italy'),
-   new players('ITA 9', 'Italy'),
    new players('mesney', 'Canada'),
    new players('Sm4ck', 'Israel'),
    new players('Miinato', 'Algeria'),
@@ -50,6 +46,7 @@ const newPlayers = [
    new players('Rick1985', 'United-States'),
    new players('ACS v2', 'United-States'),
    new players('XXSPINWHEELXX', 'United-States'),
+   new players('didisiah', 'United-States'),
 ];
 
 class matches {
